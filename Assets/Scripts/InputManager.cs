e277f23fd5e45f13498d59a3ee0113dce7e9aa6c
JPR <72471169+jeromereddy9@users.noreply.github.com>
2025-09-11 21:14:40 +0200

    Update to player character

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,69 +1,79 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Interactions;
 
 namespace FacilityZero.Manager
 {
+    [RequireComponent(typeof(PlayerInput))]
     public class InputManager : MonoBehaviour
     {
-        [SerializeField] private PlayerInput PlayerInput;
-
         public Vector2 Move { get; private set; }
-
         public Vector2 Look { get; private set; }
-
         public bool Run { get; private set; }
+        public bool CombatTogglePressed { get; set; }
 
-        private InputActionMap actionMap;
-
+        private PlayerInput playerInput;
         private InputAction moveAction;
         private InputAction lookAction;
         private InputAction runAction;
+        private InputAction combatModeAction;
 
         private void Awake()
         {
-            actionMap = PlayerInput.currentActionMap;
+            HideCursor();
+
+            playerInput = GetComponent<PlayerInput>();
+            var actionMap = playerInput.currentActionMap;
 
             moveAction = actionMap.FindAction("Move");
             lookAction = actionMap.FindAction("Look");
             runAction = actionMap.FindAction("Run");
+            combatModeAction = actionMap.FindAction("Combat Mode");
 
-            moveAction.performed += onMove;
-            lookAction.performed += onLook;
-            runAction.performed += onRun;
-
-            moveAction.canceled += onMove;
-            lookAction.canceled += onLook;
-            runAction.canceled += onRun;
-
-        }
-
-        private void onMove(InputAction.CallbackContext context)
-        {
-            Move = context.ReadValue<Vector2>();
-        }
-
-        private void onLook(InputAction.CallbackContext context)
-        {
-            Look = context.ReadValue<Vector2>();
-        }
-
-        private void onRun(InputAction.CallbackContext context)
-        {
-            Run = context.ReadValueAsButton();
+            // Combat toggle triggers once per press
+            combatModeAction.performed += ctx => CombatTogglePressed = true;
         }
 
         private void OnEnable()
         {
-            actionMap.Enable();
+            moveAction.performed += OnMove;
+            moveAction.canceled += OnMove;
+
+            lookAction.performed += OnLook;
+            lookAction.canceled += OnLook;
+
+            runAction.performed += OnRun;
+            runAction.canceled += OnRun;
+
+            playerInput.actions.Enable();
         }
 
         private void OnDisable()
         {
-            actionMap.Disable();    
+            moveAction.performed -= OnMove;
+            moveAction.canceled -= OnMove;
+
+            lookAction.performed -= OnLook;
+            lookAction.canceled -= OnLook;
+
+            runAction.performed -= OnRun;
+            runAction.canceled -= OnRun;
+
+            playerInput.actions.Disable();
+        }
+
+        private void OnMove(InputAction.CallbackContext context) =>
+            Move = context.ReadValue<Vector2>();
+
+        private void OnLook(InputAction.CallbackContext context) =>
+            Look = context.ReadValue<Vector2>();
+
+        private void OnRun(InputAction.CallbackContext context) =>
+            Run = context.ReadValueAsButton();
+
+        private void HideCursor()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
         }
     }
-
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,136 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+using FacilityZero.Manager;
 
 namespace FacilityZero.PlayerControl
 {
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(InputManager))]
     public class PlayerController : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
+        [Header("Camera Settings")]
+        [SerializeField] private Transform CameraRoot;
+        [SerializeField] private Transform Camera;
+        [SerializeField] private float UpperLimit = -40.0f;
+        [SerializeField] private float BottomLimit = 55.0f;
+        [SerializeField] private float MouseSensitivity = 2.0f;
+        [SerializeField] private float runFOV = 75f;
+        [SerializeField] private float fovLerpSpeed = 5f;
+        [SerializeField] private float bobFrequency = 6f;
+        [SerializeField] private float bobAmplitude = 0.05f;
 
+        [Header("Movement Settings")]
+        [SerializeField] private float AnimationBlendSpeed = 8.9f;
+        [SerializeField] private float WalkSpeed = 2.0f;
+        [SerializeField] private float RunSpeed = 6.0f;
+
+        private Camera cam;
+        private float defaultFOV;
+        private float bobTimer;
+
+        private Rigidbody playerRB;
+        private InputManager inputManager;
+        private Animator animator;
+        private bool hasAnimator;
+
+        private int x_velocityHash;
+        private int y_velocityHash;
+
+        private float x_Rotation;
+        private Vector2 currentVel;
+
+        private void Start()
+        {
+            hasAnimator = TryGetComponent(out animator);
+            playerRB = GetComponent<Rigidbody>();
+            inputManager = GetComponent<InputManager>();
+
+            cam = Camera.GetComponent<Camera>();
+            defaultFOV = cam.fieldOfView;
+
+            x_velocityHash = Animator.StringToHash("X_Velocity");
+            y_velocityHash = Animator.StringToHash("Y_Velocity");
         }
 
-        // Update is called once per frame
-        void Update()
-        {
+        private void FixedUpdate() => Move();
 
+        private void LateUpdate()
+        {
+            CameraMovement();
+            HandleFOV();
+            HandleHeadBob();
+            ToggleCombatMode();
+        }
+
+        // Toggle-combat system
+        private void ToggleCombatMode()
+        {
+            if (inputManager.CombatTogglePressed)
+            {
+                bool current = animator.GetBool("IsInCombat");
+                animator.SetBool("IsInCombat", !current);
+                inputManager.CombatTogglePressed = false; 
+            }
+        }
+
+        private void HandleFOV()
+        {
+            float targetFOV = inputManager.Run ? runFOV : defaultFOV;
+            cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, fovLerpSpeed * Time.deltaTime);
+        }
+
+        private void HandleHeadBob()
+        {
+            Vector3 basePos = CameraRoot.position;
+
+            if (inputManager.Move != Vector2.zero)
+            {
+                float speedFactor = inputManager.Run ? 1.0f : 0.5f;
+                bobTimer += Time.deltaTime * bobFrequency * speedFactor;
+
+                float bobOffsetY = Mathf.Sin(bobTimer) * bobAmplitude * speedFactor;
+                float bobOffsetX = Mathf.Cos(bobTimer * 0.5f) * (bobAmplitude * 0.5f);
+
+                Camera.position = basePos + new Vector3(bobOffsetX, bobOffsetY, 0);
+            }
+            else
+            {
+                bobTimer = 0f;
+                Camera.position = basePos; // reset to root
+            }
+        }
+
+        private void Move()
+        {
+            float targetSpeed = inputManager.Run ? RunSpeed : WalkSpeed;
+            if (inputManager.Move == Vector2.zero)
+                targetSpeed = 0.0f;
+
+            currentVel.x = Mathf.Lerp(currentVel.x, inputManager.Move.x * targetSpeed, AnimationBlendSpeed * Time.deltaTime);
+            currentVel.y = Mathf.Lerp(currentVel.y, inputManager.Move.y * targetSpeed, AnimationBlendSpeed * Time.deltaTime);
+
+            Vector3 targetVelocity = transform.TransformDirection(new Vector3(currentVel.x, 0, currentVel.y));
+            Vector3 velocityChange = targetVelocity - playerRB.velocity;
+            velocityChange.y = 0;
+
+            playerRB.AddForce(velocityChange, ForceMode.VelocityChange);
+
+            if (hasAnimator)
+            {
+                animator.SetFloat(x_velocityHash, currentVel.x);
+                animator.SetFloat(y_velocityHash, currentVel.y);
+            }
+        }
+
+        private void CameraMovement()
+        {
+            float mouseX = inputManager.Look.x;
+            float mouseY = inputManager.Look.y;
+
+            x_Rotation -= mouseY * MouseSensitivity * Time.deltaTime;
+            x_Rotation = Mathf.Clamp(x_Rotation, UpperLimit, BottomLimit);
+
+            Camera.localRotation = Quaternion.Euler(x_Rotation, 0, 0);
+            transform.Rotate(Vector3.up, mouseX * MouseSensitivity * Time.deltaTime);
         }
     }
 }
